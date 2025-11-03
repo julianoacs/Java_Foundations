c15b503e3644faf6744ea77ebeac06fc39d3491b
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2025-11-03 13:49:43 -0300

    Course ATT

---
+++ exercicios_secao_1/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+
+public class HelloWorld {
+    /* Exemplo oracle */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ exercicios_secao_1/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    /* Juliano */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        /*System.out.println("Hello")*/
+        /*system.out.println("Hello");*/
+    }
+}
