cffa76a6a0f8fd0f24fa05551ae27cab93640483
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2025-11-03 21:51:02 -0300

    Course ATT

---
+++ exercicios_secao_2/ProblemSet2_1/ProblemSet2_1.java
@@ -0,0 +1,14 @@
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        //Use 8 print statements to print a smiley face.
+        //Your art will rely on only a single character, besides space, such as X or #.
+        System.out.println("       ********     ")
+        System.out.println("     *          *   ")
+        System.out.println("    *   0     0   * ")
+        System.out.println("    *       L     * ")
+        System.out.println("    *    *____*   * ")
+        System.out.println("     *           *  ")
+        System.out.println("       *        *   ")
+        System.out.println("         ******     ")
+    }   
+}
